27cdf4fceef0ba53680785a9b3e3dd42eceb4b44
RunnerWolf25 <liam@limar.tk>
2021-02-20 15:55:13 +0000

    test lambda

--- cpp/mmio/src/main.cpp
+++ cpp/mmio/src/main.cpp
@@ -16,41 +16,61 @@ void msleep( uint_fast16_t msec ) {
 	}
 }
 
+void test_function( uint32_t* const io_base, const int function ) {
+for( uint_fast8_t pin = 0; pin < 28; pin++ ) { // all pins
+	for( uint_fast8_t value = 0; value <= 1; value++) { // both values
+			bool wr_value = value;
+			printf("%2u=%u\t", pin, value );
+			peripheral::gpio_fs( io_base, pin, function );
+			peripheral::gpio_write( io_base, pin, wr_value );
+			msleep(10);
+			bool rd_value = peripheral::gpio_read( io_base, pin );
+			if ( wr_value == rd_value )
+				printf("success                ");
+			else
+				printf("written %d, but read %d", wr_value, rd_value );
+			printf("\t");
+		}
+		printf("\n");
+	}
+};
+
+void test_pin( uint32_t* const io_base, const int pin ) {
+	for( uint_fast8_t value = 0; value <= 1; value++) { // both values
+		bool wr_value = value;
+		printf("%2u=%u\t", pin, value );
+		peripheral::gpio_fs( io_base, pin, peripheral::out );
+		peripheral::gpio_write( io_base, pin, wr_value );
+		msleep(10);
+		bool rd_value = peripheral::gpio_read( io_base, pin );
+		if ( wr_value == rd_value )
+			printf("success                ");
+		else
+			printf("written %d, but read %d", wr_value, rd_value );
+		printf("\t");
+	}
+	printf("\n");
+};
+
+
 int main() {
 	namespace peri=peripheral;
-	namespace addr=peri::address;
+	namespace addr=peripheral::address;
 	// get access to memory-mapped gpio gregion
-	uint32_t* const io_base = peri::map_io();
+	uint32_t* const io_base = peripheral::map_io();
 
 	if( io_base == NULL || io_base == (uint32_t*)-1 ) {
 		printf( "[ERROR] peripheral::map() returned %d\n", errno );
 		return errno;
 	}
-
-	auto test = []( uint32_t* const io_base, int function ){
-	for( uint_fast8_t pin = 0; pin < 28; pin++ ) { // all pins
-		for( uint_fast8_t value = 0; value <= 1; value++) { // both values
-				bool wr_value = value;
-				printf("%2u=%u\t", pin, value );
-				peri::gpio_fs( io_base, pin, function );
-				peri::gpio_write( io_base, pin, wr_value );
-				msleep(10);
-				bool rd_value = peri::gpio_read( io_base, pin );
-				if ( wr_value == rd_value )
-					printf("success                ");
-				else
-					printf("written %d, but read %d", wr_value, rd_value );
-				printf("\t");
-			}
-			printf("\n");
-		}
-	};
-	test( io_base, peri::out);
+	test_pin( io_base, 2);
 	puts("------------------------------");
-	test( io_base, peri::in);
+	test_function( io_base, peripheral::out);
+	puts("------------------------------");
+	test_function( io_base, peripheral::in);
 
 	// cleanup
-	peri::unmap_io( io_base ); // made it explicit for sake of making it clear
+	peripheral::unmap_io( io_base ); // made it explicit for sake of making it clear
 	                             // this is an invalid pointer now.
 	return 0;
 }
